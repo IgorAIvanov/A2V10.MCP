f5d4f375262693a4ba576b397b1ac6229eeaa183
igova <igovano@gmail.com>
2026-03-13 08:43:35 +0200

    Добавлено авторство

--- Tools/DBTools/ExecuteSQL.cs
+++ Tools/DBTools/ExecuteSQL.cs
@@ -1,8 +1,6 @@
 ﻿
 using ModelContextProtocol.Protocol;
 using ModelContextProtocol.Server;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
 using System.Text.Json;
